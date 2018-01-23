3b75a7012c12678817ac0752ece615dfe8dd0387
codest <zjtopspeed@gmail.com>
2018-01-23 14:48:43 +0800

    .

--- Code/HttpHandlerAndModule/b.aspx.cs
+++ Code/HttpHandlerAndModule/b.aspx.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace HttpHandlerAndModule
 {
-    public partial class b : System.Web.UI.Page
+    public partial class b : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
