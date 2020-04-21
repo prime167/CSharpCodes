edc426a24d8cdfaf4d13dd376deadc6e6dd2f034
codest <3722845+prime167@users.noreply.github.com>
2020-04-21 09:02:18 +0800

    a

--- Code/CSharp/Program.cs
+++ Code/CSharp/Program.cs
@@ -48,6 +48,46 @@ namespace Csharp
             bytes[2] = 45; // OK
             Assert.Equal(arr[2], bytes[2]);
             Assert.Equal(45, arr[2]);
+
+            string[] words = new string[]
+            {
+                // index from start    index from end
+                "The",      // 0                   ^9
+                "quick",    // 1                   ^8
+                "brown",    // 2                   ^7
+                "fox",      // 3                   ^6
+                "jumped",   // 4                   ^5
+                "over",     // 5                   ^4
+                "the",      // 6                   ^3
+                "lazy",     // 7                   ^2
+                "dog"       // 8                   ^1
+            };              // 9 (or words.Length) ^0
+
+            Console.WriteLine($"The last word is {words[^1]}");
+
+            string[] quickBrownFox = words[1..4];
+            foreach (var word in quickBrownFox)
+                Console.Write($"< {word} >");
+            Console.WriteLine();
+
+            //string[] lazyDog = words[^2..^0];
+            string[] lazyDog = words[^2..];
+            foreach (var word in lazyDog)
+                Console.Write($"< {word} >");
+            Console.WriteLine();
+
+            string[] allWords = words[..]; // contains "The" through "dog".
+            string[] firstPhrase = words[..4]; // contains "The" through "fox"
+            string[] lastPhrase = words[6..]; // contains "the, "lazy" and "dog"
+            foreach (var word in allWords)
+                Console.Write($"< {word} >");
+            Console.WriteLine();
+            foreach (var word in firstPhrase)
+                Console.Write($"< {word} >");
+            Console.WriteLine();
+            foreach (var word in lastPhrase)
+                Console.Write($"< {word} >");
+            Console.WriteLine();
         }
 
         private static void Print(Dog d)
