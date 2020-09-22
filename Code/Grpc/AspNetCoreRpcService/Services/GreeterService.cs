e98551e21ab2d193bc9361e300a39db8b32e3ae8
codest <3722845+prime167@users.noreply.github.com>
2020-09-22 09:44:52 +0800

    .

--- Code/Grpc/AspNetCoreRpcService/Services/GreeterService.cs
+++ Code/Grpc/AspNetCoreRpcService/Services/GreeterService.cs
@@ -10,6 +10,7 @@ namespace AspNetCoreGrpcService.Services
     public class GreeterService : Greeter.GreeterBase
     {
         private readonly ILogger<GreeterService> _logger;
+
         public GreeterService(ILogger<GreeterService> logger)
         {
             _logger = logger;
