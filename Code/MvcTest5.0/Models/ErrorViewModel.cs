3efac0b94ae4c0d6a8afd8cd06189f41de85a9c1
codest <3722845+prime167@users.noreply.github.com>
2021-07-09 14:07:47 +0800

    asp.net core 5.0 demo

---
+++ Code/MvcTest5.0/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MvcTest50.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
